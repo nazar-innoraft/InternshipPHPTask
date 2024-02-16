cc48b34d000719ac4bd0bc6804fa2a840f578472
nazar-innoraft <nazar.ali@innoraft.com>
2024-02-16 19:22:19 +0530

    Task 1 of form completed

--- index.php
+++ index.php
@@ -7,7 +7,28 @@
     <title>Form</title>
     <link rel="stylesheet" href="style.css">
 </head>
+    <form id="form" action="index.php" method="post">
+        <label for="fname">First Name:</label> <br>
+        <input type="text" name="fname" id="fname" pattern="[A-za-z]{2-20}" required> <br>
+        <label for="lname">Last Name:</label> <br>
+        <input type="text" name="lname" id="lname" pattern="[A-za-z]{2-20}" required> <br>
+        <label for="email">E-mail:</label> <br>
+        <input type="email" name="email" id="email" required> <br>
+        <label for="phone">Phone:</label> <br>
+        <input type="tel" name="phone" id="phone" pattern="[6-9]{1}[2-9]{1}[0-9]{8}" required> <br>
+        <br>
+        <input type="submit" name="submit"> <br> <br>
+        <?php
+            if(isset($_POST["submit"])){
+                $fname = $_POST["fname"];
+                $lname = $_POST["lname"];
+                $email = $_POST["email"];
+                $phone = $_POST["phone"];
+                echo "<h2>Hi, ", $fname,  " ", $lname, "</h2><br>Your email is ", $email, "<br>Your Phonr Number is ", $phone;
+            }
 
+        ?>
+    </form>
 <body>
     
 
