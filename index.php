6d9c64c069ea36d27a73430ed20cffbdf0ec6a68
nazar-innoraft <nazar.ali@innoraft.com>
2024-02-19 12:14:04 +0530

    javascript added for fullname

--- index.php
+++ index.php
@@ -7,11 +7,16 @@
     <title>Form</title>
     <link rel="stylesheet" href="style.css">
 </head>
+<body>
     <form id="form" action="index.php" method="post">
         <label for="fname">First Name:</label> <br>
         <input type="text" name="fname" id="fname" pattern="[A-za-z]{2-20}" required> <br>
         <label for="lname">Last Name:</label> <br>
         <input type="text" name="lname" id="lname" pattern="[A-za-z]{2-20}" required> <br>
+        
+        <label for="fullname">Full Name:</label> <br>
+        <input type="text" name="fullname" id="fullname" disabled required> <br>
+
         <label for="email">E-mail:</label> <br>
         <input type="email" name="email" id="email" required> <br>
         <label for="phone">Phone:</label> <br>
@@ -26,13 +31,11 @@
                 $phone = $_POST["phone"];
                 echo "<h2>Hi, ", $fname,  " ", $lname, "</h2><br>Your email is ", $email, "<br>Your Phonr Number is ", $phone;
             }
-
         ?>
     </form>
-<body>
-    
 
-    <!-- </div> -->
+    <!-- JAVASCRIPT -->
+    <script src="script.js"></script>
 </body>
 
 </html>
---
+++ script.js
@@ -0,0 +1,12 @@
+let fname = document.querySelector("#fname");
+let lname = document.querySelector("#lname");
+
+function full() {
+    document.querySelector("#fullname").value = fname.value + " " + lname.value;
+}
+fname.addEventListener("focusout", () =>{
+    full();
+});
+lname.addEventListener("focusout", () =>{
+    full();
+});
