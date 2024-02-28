4fb9d188c1f769b5e469b7757e52faecdb84fb9e
nazar-innoraft <nazar.ali@innoraft.com>
2024-02-28 20:24:50 +0530

    FT2-77: Code modified.

--- logout.php
+++ logout.php
@@ -1,6 +1,21 @@
 <?php
 session_start();
+// Unsetting the session value.
 session_unset();
+// Destroying the session.
 session_destroy();
-echo "You are logged out <a href=\"login.php\">Click to Login</a>";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Logged out</title>
+</head>
+<body>
+  <div class="container">
+    <p>Your are logged out</p>
+    <a href="login.php">Click to Login</a>
+  </div>
+</body>
+</html>
--- pdf.php
+++ pdf.php
@@ -2,35 +2,24 @@
 
 use Fpdf\Fpdf;
 require('vendor/autoload.php');
-if ($_POST["submit"]) {
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $fullname = $fname . " " . $lname;
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
-    $uploadDir = "uploads/";
-    $imgdir = $uploadDir . basename($_FILES["image"]["name"]); 
-    if($_FILES["image"]["tmp_name"]){
-        $check = getimagesize($_FILES["image"]["tmp_name"]);
-    }  
-    // ob_start();
+function getPdf($fName, $lName, $fullName, $image, $email, $phone){
     $pdf = new Fpdf();
     $pdf->AddPage();
     $pdf->SetFont("Arial", "", 16);
     $pdf->Cell(0, 10, "Form details", 1, 1, 'C');
     $pdf->Cell(50, 10, "First Name", 1, 0);
-    $pdf->Cell(0, 10, $fname, 1, 1);
+    $pdf->Cell(0, 10, $fName, 1, 1);
     $pdf->Cell(50, 10, "Last Name", 1, 0);
-    $pdf->Cell(0, 10, $lname, 1, 1);
+    $pdf->Cell(0, 10, $lName, 1, 1);
     $pdf->Cell(50, 10, "Full Name", 1, 0);
-    $pdf->Cell(0, 10, $fullname, 1, 1);
+    $pdf->Cell(0, 10, $fullName, 1, 1);
     $pdf->Cell(50, 10, "E-mail", 1, 0);
     $pdf->Cell(0, 10, $email, 1, 1);
     $pdf->Cell(50, 10, "Phone", 1, 0);
     $pdf->Cell(0, 10, $phone, 1, 1);
-    // $pdf->Image($imgdir, 70, 90, 80, 100);
-
-    $filename = $fullname . '.pdf';
+    $pdf->Image($image, 70, 90, 80, 100);
+    $dir = 'docs/';
+    $filename = $email . '.pdf';
+    $pdf->Output("$dir.$filename",'F');
     $pdf->output();
-    // ob_end_flush();
 }
