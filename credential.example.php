e2c256f4431b64286061165ceb99494381a41721
nazar-innoraft <nazar.ali@innoraft.com>
2024-02-29 20:28:12 +0530

    FT2-77: Dummy for credential.php

---
+++ credential.example.php
@@ -0,0 +1,12 @@
+<?php
+// Session Credential
+$username = 'name';
+$password = 'pass';
+// Database Credential
+$db_host = "localhost";
+$db_username = "name";
+$db_password = "pass";
+$db_name = "db";
+// Api key for email verification.
+$api_key = "1233234567890";
+?>
