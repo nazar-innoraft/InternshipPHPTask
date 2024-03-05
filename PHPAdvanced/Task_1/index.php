79cc50dbf9e0437e1afbc132ac7dea24042a64f8
nazar-innoraft <nazar.ali@innoraft.com>
2024-03-05 14:49:02 +0530

    FT2-80: PHP Advanced Task 1 - header added.

--- PHPAdvanced/Task_1/index.php
+++ PHPAdvanced/Task_1/index.php
@@ -3,13 +3,36 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css">
   <title>Home |</title>
 </head>
-<body>
 
+<body>
+  <header>
+    <div class="upper">
+      <img src="img/innoraft-logo-big-color.png" alt="inno">
+      <div class="menu">
+        <a href=""><i class="fa fa-bars"></i></a>
+        <div class="triangle"></div>
+      </div>
+    </div>
+    <div class="lower wrapper">
+      <div class="left">
+        <h2>SERVICES</h2>
+        <div class="line"></div>
+        <p>What matters to us is the quality of our services and the way we provide it. We always want to be partners to our clients.</p>
+      </div>
+      <div class="right">
+        <img src="" alt="">
+      </div>
+    </div>
+  </header>
 
 </body>
+
 </html>
