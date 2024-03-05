1df105d880908c09c7a5cbf035f666dcca5dc36a
nazar-innoraft <nazar.ali@innoraft.com>
2024-03-05 10:46:12 +0530

    FT2-77: Added request.php file to call email validation api.

--- email_validation.php
+++ email_validation.php
@@ -1,33 +1,13 @@
 <?php
 
-require 'apiInfo.php';
+require 'credential.php';
+require 'request.php';
 
-/**
- * User's email address is validating here by calling an api and return true or false.
- *
- * @param string $email_address
- *   Email address is taking as input.
- * @return bool
- *   Returning true if email is valid else false.
- */
-function email_val(string $email_address): bool {
-	global $api_key;
-	$apikey = $api_key;
-	$curl = curl_init();
-	$url = "https://emailvalidation.abstractapi.com/v1/?api_key=". $apikey. "&email=" . $email_address;
+global $api_key;
+$apikey = $api_key;
 
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	$data = curl_exec($ch);
-	curl_close($ch);
+$url = "https://emailvalidation.abstractapi.com/v1/?api_key=" . $apikey . "&email=" . $email_address;
 
-	// Print the data out onto the page.
-	if ($data) {
-		return true;
-	} else {
-		return false;
-	}
-}
+$validation_result = json_decode(email_val($url), true);
+return $validation_result['format_valid'] && $validation_result['smtp_check'];
 ?>
