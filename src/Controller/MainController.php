51fcd0988db99684894bed24df8bf6427aa91085
nazar-innoraft <nazar.ali@innoraft.com>
2024-04-17 14:33:00 +0530

    FT2-86: Initial commit.

---
+++ src/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function index()
+    {
+        return new Response('hello, home page');
+    }
+}
